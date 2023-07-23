3aedd8ed6b97146cb0cdbfda44e5eb34b4382fad
Damar Ellis <damar.ellis22@gmail.com>
2023-07-22 21:48:23 -0400

    updates

--- my/src/pages/About.js
+++ my/src/pages/About.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Headshot from "../public/DLEllis.jpg"
+import Headshot from "./assets/images/DL.jpg";
 function About() {
     return (
     <div className="container mt-5">
       <h2 id="about">About Me</h2>
-      <h1><img src={Headshot} alt="DLEllis" class="thumbnail2" height="300" width="300" /></h1>
+      <h1><div className='img'> <img src={Headshot} alt="/" /> </div></h1>
         <h1>id="about-me"</h1>
     </div>
     )}
