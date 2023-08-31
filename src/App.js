448a09faafc07b34aaab81bc42da64a3d3f7ca12
Damar Ellis <damar.ellis22@gmail.com>
2023-08-31 14:25:27 -0400

    Deployment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/Resume"
-import './index.css';
-import Header from "./components/Header";
+import Header from "./components/Header"
+import './index.css'
 import "./App.css"
 
 
@@ -17,6 +17,7 @@ function App() {
 <Route path="/" element={<Contact />} />
 <Route path="/" element={<Portfolio />} />
 <Route path="/" element={<Resume />} />
+<Route path="/" element={<Header />} />
 </Routes>
 </>
     );
