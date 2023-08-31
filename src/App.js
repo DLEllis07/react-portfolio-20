58e761485a836c7b906955906b9fce431873c836
Damar Ellis <damar.ellis22@gmail.com>
2023-08-31 11:08:31 -0400

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume"
+import './index.css';
+import Header from "./components/Header";
+import "./App.css"
+
 
 function App() {
     return (
@@ -11,6 +16,7 @@ function App() {
 <Route path="/" element={<About/>} />
 <Route path="/" element={<Contact />} />
 <Route path="/" element={<Portfolio />} />
+<Route path="/" element={<Resume />} />
 </Routes>
 </>
     );
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Headshot from "./assets/images/DL.jpg";
+
+function About() {
+    return (
+    <div className="container mt-5">
+      <h2 id="about">About Me</h2>
+      <h1><div className='img'> <img src={Headshot} alt="/" /> </div></h1>
+        <h1>id="about-me"</h1>
+    </div>
+    )}
+
+    <p>Hello, My Name is Damar Ellis! I was born and raised in Aliquippa, PA but I currently reside in Durham, NC.
+      It took me sometime to figure out what I wanted to do with my career and after some thought, I decided to attend a full stack web development bootcamp at UNC-Chapel Hill.
+      Afterwords, I hope to get an opportunity somewhere where I can hone my skills and continue to get better.</p>
+
+export default About
